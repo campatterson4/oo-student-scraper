859100553296862551777ba3947d453b29d34c1e
Camryn <camrynpatterson@yahoo.com>
2021-01-03 12:07:02 -0600

    commit tree 55477908f6b22a203cbd3e053398960c2ab27adb

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,41 +1,41 @@
 require_relative "../lib/scraper.rb"
 require_relative "../lib/student.rb"
 require 'nokogiri'
-require 'colorize'
 
-class CommandLineInterface
-  BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
+class CommandLineInteface
+  INDEX_URL = "http://learn-co-curriculum.github.io/student-scrape-site/"
+  BASE_PROFILE_URL = "http://learn-co-curriculum.github.io/student-scrape-site/profile.html"
 
   def run
-    make_students
-    add_attributes_to_students
-    display_students
+    self.make_students
+    self.add_attributes_to_students
+    self.display_students
   end
 
   def make_students
-    students_array = Scraper.scrape_index_page(BASE_PATH + 'index.html')
+    students_array = Scraper.scrape_index_page(INDEX_URL)
     Student.create_from_collection(students_array)
   end
 
   def add_attributes_to_students
     Student.all.each do |student|
-      attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
+      profile_url = BASE_PROFILE_URL
+      attributes = Scraper.scrape_profile_page(profile_url)
       student.add_student_attributes(attributes)
     end
   end
 
   def display_students
     Student.all.each do |student|
-      puts "#{student.name.upcase}".colorize(:blue)
-      puts "  location:".colorize(:light_blue) + " #{student.location}"
-      puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
-      puts "  bio:".colorize(:light_blue) + " #{student.bio}"
-      puts "  twitter:".colorize(:light_blue) + " #{student.twitter}"
-      puts "  linkedin:".colorize(:light_blue) + " #{student.linkedin}"
-      puts "  github:".colorize(:light_blue) + " #{student.github}"
-      puts "  blog:".colorize(:light_blue) + " #{student.blog}"
-      puts "----------------------".colorize(:green)
+      puts "#{student.name.upcase}:"
+      puts "  #{student.location}"
+      puts "  #{student.profile_quote}"
+      puts "  #{student.bio}"
+      puts "  #{student.twitter}"
+      puts "  #{student.linkedin}"
+      puts "  #{student.github}"
+      puts "  #{student.blog}"
+      puts "----------------------"
     end
   end
-
-end
+end 
